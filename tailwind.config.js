53ed839ad4173a5fe72b0db4afed2074c9998f48
Filla Ramadhan <fillar71@gmail.com>
2025-11-08 02:07:22 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,23 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: { extend: {} },
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#2563eb", // biru modern
+        secondary: "#1e293b", // abu tua
+        accent: "#38bdf8", // biru terang
+      },
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+      boxShadow: {
+        soft: "0 4px 20px rgba(0,0,0,0.08)",
+      },
+    },
+  },
   plugins: [],
 };
